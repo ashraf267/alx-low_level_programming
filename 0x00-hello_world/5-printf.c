167442b3f64777cf29c78f4b1af03d3914ece90a
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-08 18:21:35 -0700

    Fix #3 for task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - Prints "with proper grammer, but the outcome is a piece of art\n"
- *
- * Return: Always 0
- */
+  * main - Prints "with proper grammer, but the outcome is a piece of art,
+"
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-        printf("with proper grammer, but the outcome is a piece of art,\n");
-        return (0);
+
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
 }
