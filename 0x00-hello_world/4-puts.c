893213b05e6f116d5fbeba753b9b937bb8dc02fa
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-09 01:30:20 +0100

    Fix #2 for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/**
+  * main - Prints "Programming is like building a multilingual puzzle\n
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
+	
 	return (0);
 }
