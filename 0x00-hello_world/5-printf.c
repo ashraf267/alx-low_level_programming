679ce2539524ffa60db18b5a099e755e30e0243c
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-09 01:43:32 +0100

    Task 5 status: done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints "with proper grammer, but the outcome is a piece of art\n"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
