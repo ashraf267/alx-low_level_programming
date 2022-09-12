f8765886b66b12b72988631a725e877e159c2178
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-12 13:05:26 -0700

    Task 8 status: done and dusted

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* More headers can be added here */
+
+/**
+ * main - Prints all numbers and letters (lowercase) of base 16
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n; /* n for numbers of base 16 */
+
+	int l_l; /* l_l for lowercase letters of base 16 */
+
+	n = 48;
+	l_l = 97;
+	while (n <= 57) {
+		putchar(n);
+		n++;
+	}
+	while (l_l <= 102) {
+		putchar(l_l);
+		l_l++;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -13,3 +13,5 @@
 6-print_numberz.c
 
 7-print_tebahpla.c
+
+8-print_base16.c
