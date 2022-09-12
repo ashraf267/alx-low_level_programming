54cf0bfa268949fcf479a00edce575d952264396
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-12 12:32:01 -0700

    Task 6 status: done and dusted

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* More headers can be added here */
+
+/**
+ * main - Same as before, just different rules
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -9,3 +9,5 @@
 4-print_alphabt.c
 
 5-print_numbers.c
+
+6-print_numberz.c
