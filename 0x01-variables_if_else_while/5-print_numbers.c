cc786f12a14edd6bf861a2795e2d0773eeef92a8
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-12 20:18:55 +0100

    Fix #4 for 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+/* More headers can be added here */
 
 /**
- * main - Prints numbers less than 10, starting from 0
- *
- * Return: Always 0
- */
+  * main - Prints all numbers less than 10, starting from 0
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	int i;
-	
-	i = 0;
-	while (i < 10)
+	int n;
+
+	n = 0;
+	while(n < 10)
 	{
-		printf("%d", i);
-		i++;
+		printf("%d", n);
+		n++;
 	}
 	printf("\n");
 	return (0);
