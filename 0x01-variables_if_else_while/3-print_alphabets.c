3b76ae5a84accb96d9f337591ad39a0205b6c14f
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-12 15:50:32 +0100

    Fix #1 for 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-	charsLowercase = 97; /* ends at 122 */
-	charsUppercase = 65; /* ends at 90 */
+	char charsLowercase = 97; /* ends at 122 */
+	char charsUppercase = 65; /* ends at 90 */
 
 	/* Starts printing lowercase alphabets */
 	while (charsLowercase <= 122)
