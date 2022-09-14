a7066c06cc3b1fb6a1659a602b93194dbdf01621
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-14 02:14:43 +0100

    Okay, 5-sign.c is now dusted. Fix #1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign: Prints the sign of a number
+ * print_sign- Prints the sign of a number
  *
  * @n: The number to be checked
  * Return: If condition is true, return 1 and print +,
