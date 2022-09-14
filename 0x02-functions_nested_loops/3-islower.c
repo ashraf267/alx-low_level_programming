9a9271855dcf90d243c44a893559f23adfd0df13
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-14 01:58:28 +0100

    Fix #3 to 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /* Add extra header files here */
 
 /**
- * isLower - Checks for lowercase character
+ * islower - Checks for lowercase character
  * @c: The character to be checked
  *
  * Return: If lowercase, return 1, else, return 0
