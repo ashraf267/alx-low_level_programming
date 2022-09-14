0cd0918127653e3c5b50b6a0882945df561f6284
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-14 03:38:09 +0100

    Task 10 status: done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Adds two integers
+  * @a: parameter one
+  * @b: parameter two
+  *
+  * Return: Returns the computed sum
+  */
+int add(int a, int b)
+{
+  return (a + b);
+}
