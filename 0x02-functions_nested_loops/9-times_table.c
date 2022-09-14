40d3ecfeef9c6b8b39741b53d0e8996d29de42e1
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-13 19:22:50 -0700

    Task 9 status: almost done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0
+ *
+ * Return: Nothing
+ */
+void times_table(void)
+{
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -15,3 +15,5 @@
 7-print_last_digit.c
 
 8-24_hours.c
+
+9-times_table.c
