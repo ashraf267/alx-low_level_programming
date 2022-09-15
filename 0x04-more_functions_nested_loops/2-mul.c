26f91093acbb6138f1b4ff9658e6064a9a8dde2e
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-16 00:22:34 +0100

    Task 2 status: done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: parameter one
+ * @b: parameter two
+ *
+ * Return: Returns the computed sum
+ */
+int mul(int a, int b)
+{
+	return (a + b);
+}
