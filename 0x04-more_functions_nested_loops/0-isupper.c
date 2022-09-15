e5faa58a04bb84e62e2402334834023a9b259127
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-15 23:47:50 +0100

    Task 0 status: done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+/* Add extra header files here */
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: The character to be checked
+ *
+ * Return: if uppercase, return 1, else, return 0
+ */
+int _isupper(int c)
+{
+	int output;
+
+	if (isupper(c))
+	{
+		output = 1;
+	} else
+	{
+		output = 0;
+	}
+  return (output);
+}
