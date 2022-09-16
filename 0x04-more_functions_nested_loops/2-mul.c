ed76d79ea32a259fc14649aef71e0cbc2b7be44d
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-16 15:24:24 -0700

    Task 2: fix #4

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,10 +2,10 @@
 
 /**
  * mul - multiplies two integers
- * @a: parameter one
- * @b: parameter two
+ * @a: parameter 1
+ * @b: parameter 2
  *
- * Return: Returns the computed product
+ * Return: returns the computed product
  */
 int mul(int a, int b)
 {
