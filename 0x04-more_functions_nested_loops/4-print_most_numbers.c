3d988c5cd0fc584b671d26fccc4c7922e23f0450
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-16 15:36:10 -0700

    Task 4: fix #4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers 0 to 9 without 2 and 4, followed by a new line
+ * print_most_numbers - prints numbers 0 to 9
+ * without 2 and 4, followed by a new line
  *
  * Return: 0
  */
