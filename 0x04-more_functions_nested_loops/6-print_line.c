61de4ca208a260d086357152daec8fd6f5870185
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-15 17:40:41 -0700

    Task 6 status: done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/* Add extra headers here */
+
+/**
+ * print_line - draws a staright line in the terminal n number
+ * of times
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i <= 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
