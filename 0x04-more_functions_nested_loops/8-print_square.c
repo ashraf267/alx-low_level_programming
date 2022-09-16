f1463d467da2317bf8bba392f121f98d625e5bc6
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-15 18:39:55 -0700

    Task 8 status: done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/* Add extra headers here */
+
+/**
+ * print_square - prints a square, followed by a new line
+ * @size: size of the square
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int size_x;
+
+	size_x = size; // size assumed to be size_y
+
+	int x, y;
+
+	for (x = 0; x < size_x; x++)
+	{
+		if (size <= 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		for (y = 0; y < size; y++)
+		{
+			_putchar('#');
+		}
+		_putchar('#');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
