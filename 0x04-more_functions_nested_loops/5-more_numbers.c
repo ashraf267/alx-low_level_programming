ff8a57a03f3bd46844dcd179982156617fbafdaf
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-15 17:30:06 -0700

    Task 5: fix #1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,9 +17,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
