c884e424ee45af5100360752cd2b16daa1e4a9ea
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-16 01:02:36 +0100

    Task 4 status: done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* Add extra headers here */
+
+/**
+ * print_numbers - prints numbers 0 to 9, followed by a new line
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+    if (i == 50 || i == 52)
+    {
+      continue; /* Skip 2 and 4 */
+    }
+		_putchar(i);
+	}
+	_putchar('\n');
+}
