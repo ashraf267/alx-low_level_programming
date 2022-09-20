c228926d1b59c0b8eb49d8823466b0162a232947
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-20 16:56:10 -0700

    Task 7 status: done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to be printed
+ *
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	int len;
+
+	int n;
+
+	i = 0;
+	len = 0;
+	while (str[i++])
+	{
+		len++;
+	}
+	if ((len % 2) == 0)
+	{
+		n = len / 2;
+	} else
+	{
+		n = (len + 1) / 2;
+	}
+	for (i = n; i < len; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
