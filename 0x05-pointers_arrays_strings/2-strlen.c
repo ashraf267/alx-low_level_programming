7fa640c83b7bfb00efa37a4bb8306d721cae8452
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-20 14:43:28 -0700

    Task 2 status: done and dusted

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/* Add extra headers here */
+
+/**
+ * _strlen - returns the length of a string
+ * @s: given string
+ *
+ * Return: lenght of the given string
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
