0c67145267093254d76e5c5f928c574887030a0d
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-20 14:33:20 -0700

    Task 0: fix #3

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int
  * as parameter and updates the value it points to to 98
- * @*n: pointer to an int parameter
+ * @n: pointer to an int parameter
  *
  * Return: 0
  */
