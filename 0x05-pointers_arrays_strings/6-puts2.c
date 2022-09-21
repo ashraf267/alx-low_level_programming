48deafa9503b42624090ec0bb628dbfc964a0b51
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-20 17:27:43 -0700

    Task 6 status: done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line
+ * @str: the given input
+ *
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	int len;
+
+	i = 0;
+	len = 0;
+	while (str[i])
+	{
+		len++;
+	}
+	for (i; i < len; i += 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
