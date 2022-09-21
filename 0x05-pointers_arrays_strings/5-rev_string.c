931eabbd7c8e85c8db940b63abdaa1efade198f0
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-20 19:09:49 -0700

    Task 5 status: done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,3 +7,23 @@
  * Return: 0
  */
 void rev_string(char *s)
+{
+	int len;
+
+	int i;
+
+	char tmp;
+
+	i = 0;
+	len = 0;
+	while (s[i++])
+	{
+		len++;
+	}
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
+}
