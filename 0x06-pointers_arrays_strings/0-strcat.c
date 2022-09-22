ac385c8eb03c6ec0c54ffd4f822aa5dfd8d01f1e
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-22 01:16:15 -0700

    Task 0 status: done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	int dLen = 0;
+
+	while (dest[i++])
+	{
+		dLen++;
+	}
+
+	for (i = 0; src[i++]; i++)
+	{
+		dest[dLen++] = src[i];
+	}
+
+	return (dest);
+}
