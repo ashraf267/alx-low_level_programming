1ce865a01bd7e930074c0df8cfac2712014cdf88
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-22 13:16:15 -0700

    Task 7: fix #1

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,12 +13,14 @@ char *leet(char *str)
 	int j;
 
 	i = 0;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	char leet[8] = {'O', 'L', '?',
+	'E', 'A', '?', '?', 'T'};
 	while (str[i])
 	{
 		for (j = 0; j <= 7; j++)
 		{
-			if (str[i] == leet[j] || str[i] - 32 == leet[j])
+			if (str[i] == leet[j] ||
+			str[i] - 32 == leet[j])
 			{
 				str[i] = j + '0';
 			}
