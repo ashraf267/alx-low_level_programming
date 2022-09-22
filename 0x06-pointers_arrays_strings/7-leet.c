67e153c8e803c77ca8eaf5b373741143d009369d
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-22 11:53:08 -0700

    Task 7 status: done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: given string to be encoded
+ *
+ * Return: a pointer to the encoded string
+ */
+char *leet(char *str)
+{
+	int i;
+
+	int j;
+
+	i = 0;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	while (str[i])
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			if (str[i] == leet[j] || str[i] - 32 == leet[j])
+			{
+				str[i] = j + '0';
+			}
+		}
+		i++;
+	}
+	return (str);
+}
