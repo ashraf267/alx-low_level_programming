8e3ad855b98f7323e3e4fb6299464612c4d8069b
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-22 05:48:46 -0700

    Task 1: fix #2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strncat - concatenates two strings
+ * _strncat - concatenates two strings
  * @dest: destination string
  * @src: source string
  * @n: at most n bytes from src to dest
