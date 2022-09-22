8645297d06e70c6a289aa77432ece7db823aa803
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-22 13:26:02 -0700

    Task 7: fix #2

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#included "main.h"
 
 /**
  * leet - encodes a string into 1337
  * @str: given string to be encoded
  *
- * Return: a pointer to the encoded string
+ * Return: pointer to the encoded string
  */
 char *leet(char *str)
 {
@@ -13,14 +13,12 @@ char *leet(char *str)
 	int j;
 
 	i = 0;
-	char leet[8] = {'O', 'L', '?',
-	'E', 'A', '?', '?', 'T'};
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 	while (str[i])
 	{
 		for (j = 0; j <= 7; j++)
 		{
-			if (str[i] == leet[j] ||
-			str[i] - 32 == leet[j])
+			if (str[i] == leet[j] || str[i] - 32 == leet[j])
 			{
 				str[i] = j + '0';
 			}
