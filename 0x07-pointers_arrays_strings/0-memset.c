c01c748bdfdbebda5c76009d23c1af1c438598a2
Replit user <>
2022-09-27 04:13:55 +0000

    Task 0 status: done. Plus, I edited _putchar and main.h

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _memset - fills memory with a constant byte
+  * @s: given input char pointer
+  * @b: given input char
+  * @n: given input int
+  *
+  * Return: a pointer to the memory area s
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+  unsigned int i;
+
+  unsigned char *mem = s;
+
+  unsigned char val = c;
+
+  for (i = 0; i < n; i++)
+    mem[i] = val;
+
+  return (mem);
+}
