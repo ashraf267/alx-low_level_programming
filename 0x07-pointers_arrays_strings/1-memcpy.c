866d83eefcbe1b2ddbebaa20a815e5ef296532b9
Replit user <>
2022-09-27 04:24:11 +0000

    Task 1 status: done

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _memcpy - copies memory area
+  * @dest: destination
+  * @src: source
+  * @n: number of bytes to copy from src
+  *
+  * Return: a pointer to dest
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+  unsigned int i;
+
+  unsigned char *destination = dest;
+
+  const unsigned char *source = src;
+
+  for (i = 0; i < n; i++)
+  {
+    destination[i] = source[i];
+  }
+  return (dest);
+}
