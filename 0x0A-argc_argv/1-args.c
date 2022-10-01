0093af4f481be8ce19f8e4aae25c7f0a874c9f09
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-30 21:09:37 -0700

    Task 1 status: done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the no of arguments
+ * passed into it
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
