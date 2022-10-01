7b0eed3bc1e8b492d2da85c0766098be09dee9b9
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-09-30 21:05:11 -0700

    Task 0 status: done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
