ee4746e913edef4b938a1c0d59996832786c7023
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-10-12 14:17:44 -0700

    Task 3: fix #2

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,12 @@
 #ifndef CALC_H
 #define CALC_H
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
 typedef struct op
 {
 	char *op;
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -3,3 +3,8 @@
 1-array_iterator.c
 
 2-int_index.c
+
+3-calc.h
+3-main.c
+3-op_functions.c
+3-get_op_func.c
