a4b89ebb6fd36bfbf5c6f36da1818210917e40a9
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-10-11 18:38:25 -0700

    GEN fix #2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - a dog's blueprint
- * @name: name of new dog
- * @age: age of new dog
- * @owner: owner of new dog
- *
- */
 struct dog
 {
 	char *name;
@@ -15,9 +8,6 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - typedef for struct dog
- */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
