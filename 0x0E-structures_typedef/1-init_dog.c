a8afbba778b3c32a8a662a534268fca0a5e42208
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-10-11 17:07:19 -0700

    Task 0-1: GEN fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,13 +13,12 @@
  */
 void init_void(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog mi_dog;
+	if (d != NULL)
+	{
+		d->name = name;
+		
+		d->age = age;
 
-	d = &mi_dog;
-
-	d->name = name;
-
-	d->age = age;
-
-	d->owner = owner;
+		d->owner = owner;
+	}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,25 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog's blueprint
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ *
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
