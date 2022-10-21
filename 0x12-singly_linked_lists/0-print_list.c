7c1876b3dedbfc7662ae8e1fcf28fd6291e63ec6
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-10-21 16:08:34 -0700

    Task 0 status: done

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all elements of a
+ * list_t list
+ * @h: the given list_t list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	n = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		} else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		n++;
+
+		h = h->next;
+	}
+	return (n);
+}
