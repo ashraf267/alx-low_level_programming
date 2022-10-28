c277390ac81dc97547cbe6bb5fd4607cd8585c0f
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-10-27 21:09:00 -0700

    Task 1 status: done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - a function that prints the binary
+ * representation of a given number
+ * @n: the given number
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+
+	_putchar((n & 1) + '0');
+}
