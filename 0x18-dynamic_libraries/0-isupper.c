43cc6e0779212672a91ddca8e8b5a58f3522009a
ashraf267 <otagunolaniyi7@gmail.com>
2023-06-03 19:30:40 -0700

    added a 0-isupper.c file

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper: checks for uppercase char
+ * @c: the char to be checked
+ *
+ * Return: if uppercase, return 1
+ * else, return 0
+ */
+
+int _isupper(int c) {
+	/* empty function def */
+}
